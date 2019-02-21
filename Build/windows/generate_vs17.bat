e92a57c099c3accd68bbfba560d7f2aebc450acd
Jun <tianjun1.0@gmail.com>
2019-02-20 17:25:02 -0800

    Fix reading from stdin (#10)
    Signed-off-by: Jun Tian <jun.tian@intel.com>

--- Build/windows/generate_vs17.bat
+++ Build/windows/generate_vs17.bat
@@ -1,5 +1,5 @@
 :: Copyright(c) 2019 Intel Corporation 
 :: SPDX-License-Identifier: BSD-2-Clause-Patent
 
-cmake ../.. "-GVisual Studio 15 2017 Win64" -DCMAKE_ASM_NASM_COMPILER="yasm.exe"
+cmake ../.. "-GVisual Studio 15 2017 Win64" -DCMAKE_ASM_NASM_COMPILER="yasm.exe" -DCMAKE_CONFIGURATION_TYPES="Debug;Release"
 pause
