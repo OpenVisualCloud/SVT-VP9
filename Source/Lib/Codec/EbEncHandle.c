8d1ce19ace40a3425a9a6e144515b0f1370d78df
Issael Altero <issael.altero@intel.com>
2019-06-04 04:14:10 -0700

    Spelling Sync

--- Source/Lib/Codec/EbEncHandle.c
+++ Source/Lib/Codec/EbEncHandle.c
@@ -2089,9 +2089,9 @@ static EbErrorType  verify_settings(
         SVT_LOG("Error instance %u: Source Height must be less than 4320\n", channel_number + 1);
         return_error = EB_ErrorBadParameter;
     }
-    
+
     if (sequence_control_set_ptr->max_input_luma_height % 8 != 0) {
-        SVT_LOG("Error instance %u: Source height must be a multiple of 8\n", channel_number + 1);
+        SVT_LOG("Error instance %u: Source Height must be a multiple of 8\n", channel_number + 1);
         return_error = EB_ErrorBadParameter;
     }
 
